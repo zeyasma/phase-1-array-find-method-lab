9a5d887cc2979c3527e321260b9284be7b6256b4
Zeinab Mohamed <zeinabmohameddaud1812@gmail.com>
2026-09-23 09:39:41 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record) {
+  const win = record.find(game => game.result === "W");
+  return win ? win.year : undefined;
+}
+
+module.exports = superbowlWin;
